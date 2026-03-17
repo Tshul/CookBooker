c38eb3da67a83f761a8e4eb43b1398b786c28106
T <T@DESKTOP-1B85VAV>
2026-03-16 20:30:27 -0400

    Added get all recipes to recipeModel.js

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,3 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./cookbook.db');
 module.exports = {};
+
+const getAllRecipes = (callback) => {
+   db.all("SELECT * FROM recipes", (err, rows) => {
+      callback(err, rows);
+   });
+};
+
+module.exports = { getAllRecipes };
